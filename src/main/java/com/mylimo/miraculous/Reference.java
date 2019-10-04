88c0db6fbf3fc2cda2a380468e300130745a0539
Mylimo <mylimo.channel@gmail.com>
2019-10-04 19:43:39 +0200

    test

--- src/main/java/com/mylimo/miraculous/Reference.java
+++ src/main/java/com/mylimo/miraculous/Reference.java
@@ -5,5 +5,5 @@ public class Reference
     //General
     public static final String MOD_ID = "miraculous";
     public static final String MOD_NAME = "Miraculous";
-    public static final String MOD_VERSION = "1.14.4-0.1";
+    public static final String MOD_VERSION = "1.14.4-0.2";
 }
